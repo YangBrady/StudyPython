fd50431ec169011a3bc6a9fdd41f9d3c10b7ed66
桔子汽水蓝莓味 <mooejun@163.com>
2021-02-02 17:57:37 +0800

    //== 增加受保护类型

--- python100天/Day008 面向对象编程基础/Demo02 访问可见性.py
+++ python100天/Day008 面向对象编程基础/Demo02 访问可见性.py
@@ -1,20 +1,30 @@
 class Test:
+    def __init__(self, foo, koo):
+        self.__foo = foo  # 私有
+        self._koo = koo  # 受保护，python中一般建议属性定义成受保护的
 
-    def __init__(self, foo):
-        self.__foo = foo
-
-    def __bar(self):
+    def __bar(self):  # 私有
         print(self.__foo)
         print('__bar')
 
+    def bar2(self):
+        print(self._koo)
+        print('_bar2')
+
+    @property
+    def koo(self):
+        return self._koo
+
 
 def main():
-    test = Test('hello')
+    test = Test('hello', 'world')
+    print(test.koo)
+
     # 无法访问
     test.__bar()
     print(test.__foo)
 
-    # 可以访问，但是不建议这么做
+    # 可以换一种方式直接访问私有属性或者方法，但是不建议这么做
     test._Test__bar()
     print(test._Test__foo)
 
