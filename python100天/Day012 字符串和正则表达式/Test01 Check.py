ec183d0fc673c9d38bce529870f17afbefcef208
桔子汽水蓝莓味 <mooejun@163.com>
2021-02-03 11:15:37 +0800

    :sparkles: 新增正则表达式

---
+++ python100天/Day012 字符串和正则表达式/Test01 Check.py
@@ -0,0 +1,64 @@
+import re
+
+
+def main():
+    # check_qq()
+    # check_phone()
+    # check_bug()
+    check_poem()
+
+
+def check_qq():
+    username = input('请输入用户名: ')
+    qq = input('请输入QQ号: ')
+    # match函数的第一个参数是正则表达式字符串或正则表达式对象
+    # 第二个参数是要跟正则表达式做匹配的字符串对象
+    m1 = re.match(r'^[0-9a-zA-Z_]{6,20}$', username)
+    if not m1:
+        print('请输入有效的用户名.')
+    m2 = re.match(r'^[1-9]\d{4,11}$', qq)
+    if not m2:
+        print('请输入有效的QQ号.')
+    if m1 and m2:
+        print('你输入的信息是有效的!')
+
+
+def check_phone():
+    # 创建正则表达式对象 使用了前瞻和回顾来保证手机号前后不应该出现数字
+    pattern = re.compile(r'(?<=\D)(1[38]\d{9}|14[57]\d{8}|15[0-35-9]\d{8}|17[678]\d{8})(?=\D)')
+    sentence = '''
+    重要的事情说8130123456789遍，我的手机号是13512346789这个靓号，
+    不是15600998765，也是110或119，王大锤的手机号才是15600998765。
+    '''
+    # 查找所有匹配并保存到一个列表中
+    my_list = re.findall(pattern, sentence)
+    print(my_list)
+    print('--------华丽的分隔线--------')
+    # 通过迭代器取出匹配对象并获得匹配的内容
+    for temp in pattern.finditer(sentence):
+        print(temp.group())
+    print('--------华丽的分隔线--------')
+    # 通过search函数指定搜索位置找出所有匹配
+    m = pattern.search(sentence)
+    while m:
+        print(m.group())
+        m = pattern.search(sentence, m.end())
+
+
+def check_bug():
+    sentence = '你丫是傻叉吗? 我操你大爷的. Fuck you.'
+    purified = re.sub('[操肏艹]|fuck|shit|傻[比屄逼叉缺吊屌]|煞笔',
+                      '*', sentence, flags=re.IGNORECASE)
+    print(purified)  # 你丫是*吗? 我*你大爷的. * you.
+
+
+def check_poem():
+    poem = '窗前明月光，疑是地上霜。举头望明月，低头思故乡。'
+    sentence_list = re.split(r'[，。, .]', poem)
+    while '' in sentence_list:
+        sentence_list.remove('')
+    print(sentence_list)  # ['窗前明月光', '疑是地上霜', '举头望明月', '低头思故乡']
+
+
+if __name__ == '__main__':
+    main()
