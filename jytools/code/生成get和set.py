36d029fb07fe0a3e493fd018ba1015aaa3e5a9c9
桔子汽水蓝莓味 <mooejun@163.com>
2021-02-20 14:56:02 +0800

    :sparkles: 增加对__私有属性的支持

--- jytools/code/生成get和set.py
+++ jytools/code/生成get和set.py
@@ -4,24 +4,36 @@
 
 
 class ClassName:
+    # def __init__(self):
+    #     self._link_East = []
+    #     self._link_South = []
+    #     self._link_West = []
+    #     self._link_North = []
     def __init__(self):
-        self._link_East = []
-        self._link_South = []
-        self._link_West = []
-        self._link_North = []
+        self.__link_East = []
+        self.__link_South = []
+        self.__link_West = []
+        self.__link_North = []
+
+
+def foo1():
+    clazz = ClassName()
+    for k in clazz.__dict__:
+        if k.startswith('_ClassName__'):
+            param_split = '__'
+            param_name = k[12:]
+        elif k.startswith('_'):
+            param_split = '_'
+            param_name = k[1:]
+
+        print("@property")
+        print("def " + param_name + "(self):")
+        print("\treturn self." + param_split + param_name)
+        print("@" + param_name + ".setter")
+        print("def " + param_name + "(self," + param_name + "):")
+        print("\tself." + param_split + param_name + " = " + param_name)
+        print("\t")
 
 
 if __name__ == '__main__':
-    clazz = ClassName()
-    # print(clazz.__dict__)
-    for k in clazz.__dict__:
-        if k.startswith('_'):
-            param = k[1:]
-
-        print("@property")
-        print("def " + param + "(self):")
-        print("\treturn self._" + param)
-
-        print("@" + param + ".setter")
-        print("def " + param + "(self," + param + "):")
-        print("\tself._" + param + " = " + param)
+    foo1()
