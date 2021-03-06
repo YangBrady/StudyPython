47c8437646f89384124daddcc6b9b8bec96fcf3a
mooejun <2591577190@qq.com>
2021-03-06 14:21:55 +0800

    :fire: 删除过期文件

--- project/common-gis/bin/install_libs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo "begin install python libs"
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-python -m pip install ./gdal/GDAL-3.0.4-cp37-cp37m-win_amd64.whl
